ef39d18d19136897bb4501b85786a28158e67302
Gabrielle <gabriellemaia10@hotmail.com.br>
2023-11-22 17:11:35 -0300

    flappy bird

---
+++ TP/FlappyBird-Game/Assets/Scripts/Diretor.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diretor : MonoBehaviour { 
+    [SerializeField] private GameObject imagemGameOver;
+    private Aviao aviao;
+    private Pontuacao pontuacao;
+
+    public void FinalizarJogo(){
+        Time.timeScale = 0;
+        //habilitar a imagem Game Over
+        this.imagemGameOver.SetActive(true);
+    }
+
+    private void Start(){
+        this.aviao = GameObject.FindObjectOfType<Aviao>();
+        this.pontuacao = GameObject.FindObjectOfType<Pontuacao>();
+    }
+
+    private void DestruirObstaculos(){
+        Obstaculo[] obstaculos = GameObject.FindObjectsOfType<Obstaculo>();
+        foreach(Obstaculo obstaculo in obstaculos){
+            obstaculo.Destruir();
+        }
+    } 
+
+    public void ReiniciarJogo() {
+        this.imagemGameOver.SetActive(false);
+        Time.timeScale = 1;
+        this.aviao.Reiniciar();
+        this.DestruirObstaculos();
+        this.pontuacao.Reiniciar();
+    }
+
+}
